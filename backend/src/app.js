c1e00bc491d34e5fdbe81fc158182bb1394c6cad
snsumit <ss2153436@gmail.com>
2025-01-23 14:43:45 +0530

    added files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Product } from "./models/Product.js";
+import { Product } from "./models/product.js";
 
 dotenv.config();
 

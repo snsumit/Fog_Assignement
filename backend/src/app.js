a892dc0b41a6b1357a2d7f4d6edf28257f318b26
snsumit <ss2153436@gmail.com>
2025-01-23 15:55:29 +0530

    added files

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,10 +7,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
